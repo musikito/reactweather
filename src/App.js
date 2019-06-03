efb0742e478ecf2adf8ea0fd8f221d0936bd8f7c
jose marte <tumusicalatina@gmail.com>
2019-06-03 10:30:09 -0400

    Added scss styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import "./sass/app.scss";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="app-container">
+        <div className="main-container">Main container</div>
+      </div>
+    );
   }
 }
 
---
+++ src/store.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+export default class Store extends React.Component{
+
+    constructor(props){
+        super(props);
+        //Main App States
+        this.state = {
+            appName: "Weather Up"
+        }
+    }
+    render(){
+        return React.Children.map( this.props.children,  (child)  => {
+            return React.cloneElement(child, { ...this.state });
+    });
+    }
+
+}
+
+
